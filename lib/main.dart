b9a7c930f802a7615aaaba07b41cc775369e5261
dinnizluis@gmail.com <dinnizluis@gmail.com>
2020-09-14 20:45:34 -0300

    Reorganize project files for github action setup

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:mamoneyproject/pages/authentication/signin.dart';
+
+void main() {
+  runApp(MaMoneyApp());
+}
+
+class MaMoneyApp extends StatelessWidget {
+  // This widget is the root of the application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MaMoney',
+      theme: ThemeData(
+        primaryColor: Color(0xff780444),
+        primaryColorLight: Color(0xffab3f6f),
+        primaryColorDark: Color(0xff47001d),        
+        fontFamily: 'Red Hat Display',
+        brightness: Brightness.light,
+        visualDensity: VisualDensity.adaptivePlatformDensity
+      ),
+      home: SignIn(key: Key('loginPage')),
+    );
+  }
+}
