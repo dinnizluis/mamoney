1854a85cc6efc22dc745696735ebdfe1ee8890c0
dinnizluis@gmail.com <dinnizluis@gmail.com>
2020-09-19 20:29:53 -0300

    Refactor test case to consider ScreenUtil

--- test/widgets/buttonmamoney_test.dart
+++ test/widgets/buttonmamoney_test.dart
@@ -2,9 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mamoney/widgets/buttonmamoney.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   testWidgets('Testing app component for button', (WidgetTester tester) async {
+    WidgetsFlutterBinding.ensureInitialized();
+    ScreenUtil.init(designSize: Size(750, 1334), allowFontScaling: false);
+
     //Build da tela
     await tester.pumpWidget(MaterialApp(
         home: ButtonMaMoney(
